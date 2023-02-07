0b3c6be2dab25391920898bd5059170fb41af10d
Fausto Bettencourt <faustobettencourt@gmail.com>
2023-02-07 16:51:11 +0000

    Database imports + Cards models and Routes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const uModel = require("../models/usersModel");
-    
-router.get('/', async function(req, res, next) {
-    try { 
-        console.log("Get all users");
-        let result = await uModel.getAll();
-        res.status(result.status).send(result.result);
-    } catch(err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
+
+router.get("/", async function (req, res, next) {
+  try {
+    console.log("Get all users");
+    let result = await uModel.getAll();
+    res.status(result.status).send(result.result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
-      
+
 module.exports = router;
