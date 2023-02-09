7835635dfc8cb3a9c987569781f01675b6885f39
Fausto Bettencourt <faustobettencourt@gmail.com>
2023-02-09 06:30:34 +0000

    Added new endpoints yet to work

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* Importes */
 require('dotenv').config(); /* Importar variaveis de ambiente do .env */
 var express = require('express'); /* Importar modulo express */
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var morgan = require('morgan');
+var path = require('path'); /* Importar modulo path */
+var cookieParser = require('cookie-parser'); /* Importar modulo cookies */
+var morgan = require('morgan'); /* Importar modulo morgan */
+const usersRouter = require("./routes/usersRoutes"); /* Conecta route users a App */
+const cardsRouter = require("./routes/cardsRoutes"); /* Conecta route cards a App */
       
 var app = express(); /* Criar aplicação */
       
@@ -13,8 +15,8 @@ app.use(express.json()); /* Parses qualquer JSON no corpo do request */
 app.use(express.urlencoded({ extended: false })); /* Parses qualquer info urlencoded no corpo do request */
 app.use(cookieParser(process.env.COOKIE_SECRET)); /* Configura-se com o COOKIE_SECRET do .env */
 app.use(express.static(path.join(__dirname, 'public'))); /* Define-se "public" a pasta dos ficheiros estaticos */
-const usersRouter = require("./routes/usersRoutes");
-app.use("/api/users",usersRouter);
+app.use("/api/users",usersRouter); /* Define Endpoint para users */
+app.use("/api/cards",cardsRouter); /* Define Endpoint para cards */
 const port = parseInt(process.env.port || '8080'); /* Port pode ser mudado ao adicionar variável ao .env */
 
 /* Correr aplicação */
